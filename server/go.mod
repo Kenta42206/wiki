3115c0edd84104c96d69a5ba8c9647de83bc980b
kenta42206 <arp42206km@gmail.com>
2024-11-24 01:13:47 +0900

    fix db connection setting for docker

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require github.com/gin-gonic/gin v1.10.0
 require (
 	github.com/aymerick/douceur v0.2.0 // indirect
 	github.com/gorilla/css v1.0.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
 
 require (
--- server/main.go
+++ server/main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
 	"wiki/server/db"
 	"wiki/server/handlers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	// データベース接続
-	// dbConf := {
-	// 	User: os.Getenv("DB_USER"),
-	// }
-	err := db.InitDB("postgresql://postgres:postgres@localhost:5432/wiki?sslmode=disable")
-	// dbConf := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
-	// 	os.Getenv("DB_USER"),
-	// 	os.Getenv("DB_PASSWORD"),
-	// 	os.Getenv("DB_HOST"),
-	// 	os.Getenv("DB_PORT"),
-	// 	os.Getenv("DB_NAME"))
-	// err := db.InitDB(dbConf)
+
+	err := godotenv.Load(".env")
+	
+	// もし err がnilではないなら、"読み込み出来ませんでした"が出力されます。
+	if err != nil {
+		slog.Error("読み込み出来ませんでした", "error", err)
+	} 
+
+	// example
+	//
+	// "postgresql://postgres:postgres@localhost:5432/wiki?sslmode=disable"
+	dbConf := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"))
+	err = db.InitDB(dbConf)
 	if err != nil {
 		slog.Error("Failed to connect to database", "error", err)
 	}
